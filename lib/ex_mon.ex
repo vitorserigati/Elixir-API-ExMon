89d6fb3ac4e1bcb9d4e13350f88854734897a1a7
Vitor Serigati <vitor_serigati@hotmail.com>
2022-06-24 23:22:21 -0300

    Criado controllers e views de Trainer. Assim como rotas para cada requisicao da api

---
+++ lib/ex_mon.ex
@@ -0,0 +1,6 @@
+defmodule ExMon do
+  alias ExMon.Trainer
+
+  defdelegate create_trainer(params), to: Trainer.Create, as: :call
+  defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
+end
