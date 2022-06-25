d7eef0a51cffaecb999ada6db97953c609691f99
Vitor Serigati <vitor_serigati@hotmail.com>
2022-06-25 17:58:30 -0300

    feat: adicionado update trainer

---
+++ lib/ex_mon/trainer/get.ex
@@ -0,0 +1,18 @@
+defmodule ExMon.Trainer.Get do
+  alias ExMon.{Trainer, Repo}
+  alias Ecto.UUID
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Incorrect UUID"}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(uuid) do
+    case Repo.get(Trainer, uuid) do
+      nil -> {:error, "Trainer not Found"}
+      trainer -> {:ok, trainer}
+    end
+  end
+end
